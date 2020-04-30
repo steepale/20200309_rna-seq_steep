27c49b39fc938ff6dadb12e3e860c0748537ef8a
steepale <alec.steep@gmail.com>
2020-04-29 20:41:34 -0400

    Update 20200426_rnaseq-kidney-female-de-c0vsc7_steep.R

--- scripts/20200426_rnaseq-kidney-female-de-c0vsc7_steep.R
+++ scripts/20200426_rnaseq-kidney-female-de-c0vsc7_steep.R
@@ -78,17 +78,20 @@ f_pmap_aslist <- function(df) {
 }
 ################################################################################
 
-# Function to relabel RNASeq read names to orthologs
+# Functions to relabel RNASeq read names to orthologs
 ###############################################################################################
 # mmusculus_gene_ensembl: Mouse genes (GRCm38.p6)
 # rnorvegicus_gene_ensembl: Rat genes (Rnor_6.0)
-rat2mouse_ortho <- function(x, column = "ENSEMBL_RAT") {
+rat_mouse_ortho <- function(x, column = "ENSEMBL_RAT", direction = 'rat2mouse') {
         # Ensure 'x' is a data.frame
         if ( class(x) != "data.frame" ) {
                 stop("'x' must be a data frame", class.= FALSE)
         }
-        if ( column != 'ENSEMBL_RAT' ){
-                stop("'column' must be 'ENSEMBL_RAT'", class.= FALSE)
+        if ( column %!in% c('ENSEMBL_RAT','ENSEMBL_MOUSE') ){
+                stop("'column' must be either 'ENSEMBL_RAT' or 'ENSEMBL_MOUSE'", class.= FALSE)
+        }
+        if ( direction %!in% c('rat2mouse', 'mouse2rat')){
+                stop("'direction' must be either 'rat2mouse' or 'mouse2rat'")
         }
         
         # Load requirements
@@ -99,26 +102,62 @@ rat2mouse_ortho <- function(x, column = "ENSEMBL_RAT") {
         mart_mm_ens = useMart("ensembl", dataset="mmusculus_gene_ensembl")
         mart_rn_ens = useMart("ensembl", dataset="rnorvegicus_gene_ensembl")
         # Create ortholog table
-        ortho_df <- getLDS(attributes=c("ensembl_gene_id","mmusculus_homolog_orthology_confidence"),
-                           filters="ensembl_gene_id", 
-                           values = x[[column]], 
-                           mart=mart_rn_ens,
-                           attributesL=c("ensembl_gene_id"), 
-                           martL=mart_mm_ens) # Use biomart to get orthologs
-        # Filter out any low confidence orthologs and any genes that are not one-to-one orthologs in both directions
-        ortho_df <- ortho_df[ortho_df$Mouse.orthology.confidence..0.low..1.high. == '1',]
-        ortho_df <- ortho_df[!duplicated(ortho_df[,1]),]
-        ortho_df <- ortho_df[!duplicated(ortho_df[,3]),]
-        names(ortho_df) <- c('ENSEMBL_RAT','CONFIDENCE','ENSEMBL_MOUSE') 
-        ortho_df <- ortho_df %>%
-                select(-CONFIDENCE)
-        # Assign the HUGO symbols to a new column
-        x <- left_join(x, ortho_df, by = 'ENSEMBL_RAT') %>%
-                filter(!is.na(ENSEMBL_RAT)) %>%
-                mutate(SYMBOL_RAT = mapIds(org.Rn.eg.db, ENSEMBL_RAT, "SYMBOL", "ENSEMBL"))
+        if (direction == 'rat2mouse'){
+                ortho_df <- getLDS(attributes=c("ensembl_gene_id",
+                                             "mmusculus_homolog_orthology_confidence"),
+                                   filters="ensembl_gene_id", 
+                                   values = x[[column]], 
+                                   mart=mart_rn_ens,
+                                   attributesL=c("ensembl_gene_id"), 
+                                   martL=mart_mm_ens) # Use biomart to get orthologs
+                # Filter out any low confidence orthologs and any genes 
+                #that are not one-to-one orthologs in both directions
+                ortho_df <- ortho_df[ortho_df$Mouse.orthology.confidence..0.low..1.high. == '1',]
+                ortho_df <- ortho_df[!duplicated(ortho_df[,1]),]
+                ortho_df <- ortho_df[!duplicated(ortho_df[,3]),]
+                names(ortho_df) <- c('ENSEMBL_RAT','CONFIDENCE','ENSEMBL_MOUSE') 
+                ortho_df <- ortho_df %>%
+                        select(-CONFIDENCE)
+                # Assign the HUGO symbols to a new column
+                x <- left_join(x, ortho_df, by = 'ENSEMBL_RAT') %>%
+                        filter(!is.na(ENSEMBL_RAT)) %>%
+                        mutate(SYMBOL_RAT = mapIds(org.Rn.eg.db, ENSEMBL_RAT, "SYMBOL", "ENSEMBL"))
+                
+        }else{
+                ortho_df <- getLDS(attributes=c("ensembl_gene_id",
+                                                "rnorvegicus_homolog_orthology_confidence"),
+                                   filters="ensembl_gene_id", 
+                                   values = x[[column]], 
+                                   mart=mart_mm_ens,
+                                   attributesL=c("ensembl_gene_id"), 
+                                   martL=mart_rn_ens) # Use biomart to get orthologs
+                # Filter out any low confidence orthologs and any genes 
+                #that are not one-to-one orthologs in both directions
+                ortho_df <- ortho_df[ortho_df$Rat.orthology.confidence..0.low..1.high. == '1',]
+                ortho_df <- ortho_df[!duplicated(ortho_df[,1]),]
+                ortho_df <- ortho_df[!duplicated(ortho_df[,3]),]
+                names(ortho_df) <- c('ENSEMBL_MOUSE','CONFIDENCE','ENSEMBL_RAT') 
+                ortho_df <- ortho_df %>%
+                        select(-CONFIDENCE)
+                # Assign the HUGO symbols to a new column
+                x <- left_join(x, ortho_df, by = 'ENSEMBL_MOUSE') %>%
+                        filter(!is.na(ENSEMBL_MOUSE)) %>%
+                        mutate(SYMBOL_RAT = mapIds(org.Rn.eg.db, ENSEMBL_RAT, "SYMBOL", "ENSEMBL"))
+        }
+        # Return the output
         x
 }
+
 #########################################################################################
+
+# Function to take lowercase strings and convert the first letter to uppercase
+################################################################################
+firstup <- function(x) {
+        substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+        x
+}
+################################################################################
+
 
 #' ## Load & Clean Data
 #' ##### Data files to load:
@@ -258,7 +297,8 @@ row.names(fkid_cols) <- fkid_cols$sample_key
 all(rownames(fkid_cols) == colnames(fkid_counts))
 
 # Create a design formula and load counts and supporting annotation into an S4 object (DESeq infrastructure)
-design = ~ controls_binary # Primary variable needs to be last less.
+design = ~animal.registration.sex + controls_binary # Primary variable needs to be last.
+design = ~controls_binary # Primary variable needs to be last.
 title = paste0('Design: ',as.character(design))
 dds1 <- DESeqDataSetFromMatrix(countData = fkid_counts,
                                colData = fkid_cols,
@@ -390,6 +430,7 @@ head(res)
 table(res$padj < 0.05)
 
 # Generate a summary of the results
+summary(res_old) # res_old has design for only primary variable of interest
 summary(res)
 
 #' ### Visualize Results: Control 0 hr vs Control 7 hr
@@ -415,7 +456,7 @@ de_df$SYMBOL_RAT <- mapIds(org.Rn.eg.db, de_df$ENSEMBL_RAT, "SYMBOL", "ENSEMBL")
 de_df$ENTREZ_RAT <- mapIds(org.Rn.eg.db, de_df$ENSEMBL_RAT, "ENTREZID", "ENSEMBL")
 
 # Convert gene symbols to mouse orthologs
-de_df <- rat2mouse_ortho(de_df, column = 'ENSEMBL_RAT')
+de_df <- rat_mouse_ortho(de_df, column = 'ENSEMBL_RAT', direction = 'rat2mouse')
 
 output_n <- de_df$ENSEMBL_RAT %>% unique() %>% length() %>% as.character()
 #' ##### High confidence ortholgos were collected from Yan et. al. (Supplementary Table 2) and converted to high confidence rat orthologs with Ensembl.
@@ -435,26 +476,14 @@ output_n <- de_df$ENSEMBL_RAT %>% unique() %>% length() %>% as.character()
 
 # COllect the gene symbols
 de_df$SYMBOL_MOUSE <- mapIds(org.Mm.eg.db, de_df$ENSEMBL_MOUSE, "SYMBOL", "ENSEMBL")
-symbol_out <- de_df %>%
+symbol_up <- de_df %>%
         filter(!is.na(SYMBOL_MOUSE)) %>%
-        select(SYMBOL_MOUSE) %>% unlist() %>% as.character()
-
-out_file <- paste0(WD,'/data/20200426_rnaseq-kidney-C0vsC7-DE-genes-q05-lfc0-mouse-symbol_steep.txt')
-write.table(symbol_out, file = out_file, sep = '\n', 
-            quote = FALSE, row.names = FALSE, col.names = FALSE)
-
-# Filter and format input for LRPath
-#LRPath_out <- de_df %>%
-#        filter(!is.na(RAT_ENTREZ)) %>%
-#        select(geneid = RAT_ENTREZ,
-#               PValue = pvalue,
-#               logFC = log2FoldChange,
-#               norm_avg_readcount = baseMean)
-
-#out_file <- paste0(WD,'/data/20200426_rnaseq-kidney-C0vsC7-DE-genes-q05-lfc0_steep.txt')
-#write.table(LRPath_out, file = out_file, sep = '\t', 
-#            quote = FALSE, row.names = FALSE, col.names = TRUE)
-
+        filter(log2FoldChange > 0) %>%
+        select(SYMBOL_MOUSE) %>% unlist() %>% unique() %>% as.character()
+symbol_down <- de_df %>%
+        filter(!is.na(SYMBOL_MOUSE)) %>%
+        filter(log2FoldChange < 0) %>%
+        select(SYMBOL_MOUSE) %>% unlist() %>% unique() %>% as.character()
 
 #' ### Pathway Enrichment Analysis (w/ Enrichr)
 
@@ -463,46 +492,58 @@ write.table(symbol_out, file = out_file, sep = '\n',
 ########### Visualize Results: Control 0 hr vs Control 7 hr  ###################
 ################################################################################
 
-# Manipulate the file in linux
-# sed "s/\#//g" 20200426_rnaseq-kidney-C0vsC7-DE-genes-q05-lfc0-lrpath_steep.txt | tr "'" "___" | sed 's/___//g' > 20200426_rnaseq-kidney-C0vsC7-DE-genes-q05-lfc0-lrpath_steep.txt
+# To check available databases from enrichr
+#dbs <- listEnrichrDbs()
+#if (is.null(dbs)) websiteLive <- FALSE
+#dbs$libraryName
 
-# Load the Enrichr results
-in_file <- paste0(WD,'/data/20200426_rnaseq-kidney-C0vsC7-DE-genes-q05-lfc0-lrpath_steep.txt')
-lrpath_res <- read.table(file = in_file, header = TRUE, sep = '\t', stringsAsFactors = FALSE)
-names(lrpath_res)
-head(lrpath_res)
+# Collect the desired pathways to query for enrichment
+desired <- c("KEGG_2019_Mouse",
+             "GO_Biological_Process_2018",
+             "GO_Molecular_Function_2018")
+
+enriched_up <- enrichr(symbol_up, desired)
+enriched_down <- enrichr(symbol_down, desired)
+n<-1
+enrichr_df <- data.frame()
+for(db in desired){
+        # Collapse the data into flat format
+        enriched_up[[n]]$Data.Base <- db
+        enriched_up[[n]]$UPDOWN <- 'Up'
+        enriched_down[[n]]$Data.Base <- db
+        enriched_down[[n]]$UPDOWN <- 'Down'
+        enrichr_df <- rbind(enrichr_df,enriched_up[[n]],enriched_down[[n]])
+        n <- n + 1
+}
+
+# Generate a new column for the number of genes per enriched term
+enrichr_df  <- enrichr_df %>% separate(Overlap, sep = "/", into = c("Overlap.N","Total.N"))
+enrichr_df$FDR_nlog <- -log(enrichr_df$Adjusted.P.value)
 
 # Filter the results
-dat <- lrpath_res %>%
-        select(Name,ConceptType, Genes,Direction,OddsRatio,P.Value,FDR, SigGenes) %>%
-        #filter(Genes < 2000 & Genes > 30) %>%
-        #filter(FDR <= 0.05) %>%
-        group_by(ConceptType, Direction) %>%
-        arrange(FDR) %>%
-        dplyr::slice(1:30) %>%
+enrichr_plot <- enrichr_df %>%
+        filter(Total.N <= 500) %>%
+        filter(Total.N >= 10) %>%
+        filter(Adjusted.P.value <= 0.05) %>%
+        arrange(desc(FDR_nlog)) %>%
+        dplyr::slice(1:40) %>%
+        group_by(Term, UPDOWN) %>%
+        arrange(FDR_nlog) %>%
         ungroup()
 
-#dot plot
-ggplot(dat[dat$ConceptType=="KEGG" & dat$Direction == "up",], 
-       aes(x = Name, y = Genes, size = OddsRatio, col = P.Value)) +
-        geom_point() +
-        coord_flip() +
-        scale_color_gradient(low = "red", high = "black") +
-        theme(axis.text = element_text(size = rel(1)))
+# Adjust levels
+enrichr_plot$UPDOWN <- factor(enrichr_plot$UPDOWN, levels = c('Up','Down'))
 
-#dot plot
-ggplot(dat[dat$ConceptType=="GOBP" & dat$Direction == "down",], 
-       aes(x = Name, y = X.Genes, size = 1/OddsRatio, col = P.Value)) +
-        geom_point() +
-        coord_flip() +
-        scale_color_gradient(low = "blue", high = "black") +
-        theme(axis.text = element_text(size = rel(1)))
+# Create a special column that will allow for proper ordering of Pathways in Plot:
+# Ordering will have 2 priorities in this order: Up/Down regulation & -Log(FDR)
+enrichr_plot <- enrichr_plot %>%
+        mutate(FDR_ord = ifelse(UPDOWN == 'Down', -FDR_nlog, FDR_nlog))
 
 # Visualize results with barplot
-ggplot(df, aes(x=Term, y=log, fill = up_dn)) +
+ggplot(enrichr_plot, aes(x=reorder(Term, FDR_ord), y=FDR_nlog, fill = UPDOWN)) +
         geom_bar(stat='identity') +
         coord_flip() +
-        ylab("-Log(FDR q-value)") +
+        ylab("-Log(FDR)") +
         xlab("") +
         theme_linedraw() +
         guides(fill=guide_legend(title="Up/Down")) +
@@ -515,4 +556,132 @@ ggplot(df, aes(x=Term, y=log, fill = up_dn)) +
               axis.title=element_text(size=12,face="bold"),
               strip.text = element_blank()) +
         theme(panel.grid.major = element_blank())
+
+#' ## Additional Test for Enrichment: Circadian Gene Set
+
+#+ Additional Test for Enrichment: Circadian Gene Set
+################################################################################
+########### Additional Test for Enrichment: Circadian Gene Set  ################
+################################################################################
+
+# Steps:
+# Logistic regression for enrichment of circadian gene set
+# Generate updated list of circadian genes
+# Geomtile plot of shared genes (bi-heatmap)
+
+# Create a dataframe for logistic regression
+ENSEMBL_RAT <- row.names(assay(rld))
+de_genes <- de_df %>%
+        filter(!is.na(ENSEMBL_RAT)) %>%
+        select(ENSEMBL_RAT) %>% unlist() %>% unique() %>% as.character()
+logreg_df <- data.frame(ENSEMBL_RAT)
+# Generate a column for circadian gene status
+logreg_df <- mutate(logreg_df, CIRC = ifelse(ENSEMBL_RAT %in% circ_kid$ENSEMBL_RAT, 1, 0))
+circ_genes <- ENSEMBL_RAT[ENSEMBL_RAT %in% circ_kid$ENSEMBL_RAT]
+# Generate a column for differential gene epression status
+logreg_df <- mutate(logreg_df, DE = ifelse(ENSEMBL_RAT %in% de_genes, 1, 0))
+# Remove gene ids and set them to rownames
+row.names(logreg_df) <- logreg_df$ENSEMBL_RAT
+logreg_df <-  logreg_df %>% select(-ENSEMBL_RAT)
+
+# Adjust column objects
+factor_cols <- colnames(logreg_df)
+for(fc in factor_cols){
+        logreg_df[[fc]] <- as.factor(logreg_df[[fc]])
+}
+
+# Fit a logistic regression model
+mod <- glm(formula = CIRC ~ DE, 
+           family = binomial(link = logit), 
+           data = logreg_df)
+summary(mod)
+
+# Perform a Chi-square analysis
+#######################
+# Create a proper data structure for chi squared
+
+de_circ <- logreg_df %>%
+        filter(CIRC == 1 & DE == 1) %>%
+        nrow()
+de_no_circ <- logreg_df %>%
+        filter(CIRC == 0 & DE == 1) %>%
+        nrow()
+no_de_circ <- logreg_df %>%
+        filter(CIRC == 1 & DE == 0) %>%
+        nrow()
+no_de_no_circ <- logreg_df %>%
+        filter(CIRC == 0 & DE == 0) %>%
+        nrow()
+x2_df <- data.frame("CIRC" = c(de_circ, no_de_circ), 
+                    "NON-CIRC" = c(de_no_circ, no_de_no_circ))
+row.names(x2_df) <- c('DE','NON-DE')
+
+# Create the proper data structure for visualization of chi square with mosaic plot
+mosaic_df <- data.frame(ENSEMBL_RAT)
+# Generate a column for circadian gene status
+mosaic_df <- mutate(mosaic_df, CIRC = ifelse(ENSEMBL_RAT %in% circ_kid$ENSEMBL_RAT, 'CIRC', "NON-CIRC"))
+# Generate a column for DE gene status
+mosaic_df <- mutate(mosaic_df, DE = ifelse(ENSEMBL_RAT %in% de_genes, 'DE', "NON-DE"))
+
+# Generate a table of results
+mosaic_tbl <- table(mosaic_df$CIRC, mosaic_df$DE, dnn=c("CIRC","DE"))
+
+# Perform Chi-squared
+#sink('')
+(( c=chisq.test(x2_df, simulate.p.value = FALSE) ))
+(( fisher.test(x2_df, simulate.p.value = FALSE) ))
+
+#sink()
+c$observed
+round(c$expected)
+
+# Visualize the chisquare analysis with a mosaic plot
+mosaic(~ CIRC + DE, data = mosaic_tbl,
+       shade = TRUE, legend = TRUE)
+
+# Update the shared list of genes
+##############################################
+logreg_df$ENSEMBL_RAT <- row.names(logreg_df)
+de_kid_circ <- logreg_df %>%
+        filter(CIRC == 1 & DE == 1) %>%
+        select(ENSEMBL_RAT) %>% unlist() %>% as.character()
+
+kegg_circ <- enrichr_df %>%
+        filter(Term == 'Circadian rhythm') %>%
+        select(Genes) %>%
+        unlist() %>% as.character() %>% 
+        str_split(';') %>% unlist() %>% unique() %>%
+        tolower() %>% firstup() %>% as.data.frame()
+names(kegg_circ) <- 'SYMBOL_MOUSE'
+# Convert the mouse symbols to rat ensembl gene ids
+kegg_circ$ENSEMBL_MOUSE = mapIds(org.Mm.eg.db, 
+                                 as.character(kegg_circ$SYMBOL_MOUSE), 
+                                 "ENSEMBL", "SYMBOL")
+kegg_circ <- rat_mouse_ortho(kegg_circ, column = 'ENSEMBL_MOUSE', direction = 'mouse2rat')
+CIRC_GENES <- c(kegg_circ$ENSEMBL_RAT[kegg_circ$ENSEMBL_RAT %!in% de_kid_circ], de_kid_circ) %>% 
+        unique()
+
+#' ## Visualize Circadian Genes Differentially Expressed
+
+#+ Visualize Circadian Genes Differentially Expressed
+################################################################################
+########### Visualize Circadian Genes Differentially Expressed  ################
+################################################################################
+
+# Steps:
+#-Barplot of DE expression between C0 and C7
+#-Geom tile heatmap of circadian genes in other pathways
+#-Heatmap and k-means/SOM clustered circadian genes in control contrast and exercsie groups
+#- Same analysis but for downstream targets of CIRC genes
+#-Next to barplot show expression of identical gene in exercise groups
+#-Perform a ttest between peaks of both genes
+#-Venn diagram and supporting qqplot of DE genes between analyses
+#Examination of genes lost
+#Can it be corrected for?
+
+
+
+
+
+
 
